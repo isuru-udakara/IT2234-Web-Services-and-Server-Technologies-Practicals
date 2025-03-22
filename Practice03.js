ed8c1f65b95913067cddfac201920a6cdd3b75d0
Isuru Udakara <pereraisuruudakara@gmail.com>
2025-03-22 12:59:21 +0530

    Created function to subtract two numbers

--- Practice03.js
+++ Practice03.js
@@ -7,3 +7,10 @@ function sum(){
     return 5 + 6;
 }
 console.log(sum());
+
+function sub(a, b){
+    return a - b;
+}
+console.log(sub(56, 34));
+console.log(sub(56, 90));
+console.log(sub(23, 90));
