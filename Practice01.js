15a9eef626e84dc68a9d26c6628d2ea0e1731d11
Isuru Udakara <pereraisuruudakara@gmail.com>
2025-03-22 14:42:46 +0530

    Defined the port 3001

--- Practice01.js
+++ Practice01.js
@@ -1,3 +1,5 @@
 const express= require("express");
 // app object represents Express application and will be used to define routes and configure the server.
 const app = express();
+// server will listen on port 3001
+const port = 3001;
