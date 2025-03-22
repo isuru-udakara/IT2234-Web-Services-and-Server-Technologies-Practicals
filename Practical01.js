3b0769d4f0c664e022da1c6e189c6b306e505d26
Isuru Udakara <pereraisuruudakara@gmail.com>
2025-03-22 12:14:20 +0530

    Created an arrow function with default parameters to find product of two numbers

--- Practical01.js
+++ Practical01.js
@@ -10,3 +10,11 @@ console.log(sum(90, 32));
 console.log(sum(89, 34));
 
 console.log("\n");
+
+// default parameters 
+const mult= (x= 20, y= 5) => {return x * y};
+console.log(mult());
+console.log(mult(90));
+console.log(mult(y= 86));
+console.log(mult(x= 80));
+console.log(mult(90, 10));
