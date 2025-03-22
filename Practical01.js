92b6ca86be320815cd2af9f90f747762f67babd6
Isuru Udakara <pereraisuruudakara@gmail.com>
2025-03-22 12:12:21 +0530

    Created an arrow function to find sum of two numbers

--- Practical01.js
+++ Practical01.js
@@ -1,3 +1,10 @@
 // arrow function
 const msg= () => {return "Hello "};
 console.log(msg());
+
+// sum two numbers 
+const sum= (x, y) => {return x + y};
+console.log(sum(23, 90));
+console.log(sum(34, 35));
+console.log(sum(90, 32));
+console.log(sum(89, 34));
