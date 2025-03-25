00ef25ff9a744f4ab90d627175dd8d0e9f226a34
Isuru Udakara <pereraisuruudakara@gmail.com>
2025-03-25 12:53:22 +0530

    Set up a route for HTTP GET requests to the root path

--- Script1.js
+++ Script1.js
@@ -2,7 +2,9 @@ const express= require("express");
 const app= express();
 const port= 3002;
 
-
+app.get('/', (req, res)=>{
+    res.send("Hello Express JS");
+});
 
 app.listen(port, ()=> {
     console.log(`Server is running on ${port}`);
