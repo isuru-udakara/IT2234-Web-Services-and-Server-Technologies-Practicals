02b0be29d1c140c3432c906250f9331b1dc711d3
Isuru Udakara <pereraisuruudakara@gmail.com>
2025-03-25 12:58:00 +0530

    Listening  for GET requests to the /msg endpoint

--- Script1.js
+++ Script1.js
@@ -6,6 +6,10 @@ app.get('/', (req, res)=>{
     res.send("Hello Express JS");
 });
 
+app.get('/msg', (req, res)=>{
+    res.send("It's me your friendly neighbourhood");
+});
+
 app.listen(port, ()=> {
     console.log(`Server is running on ${port}`);
 });
