b3634d9372a4741516d0678c51e281e16ec5ee7a
Isuru Udakara <pereraisuruudakara@gmail.com>
2025-03-25 13:10:43 +0530

    Created array with student objects

---
+++ Students.js
@@ -0,0 +1,7 @@
+let students= [
+    {id: "2021ict201", Name: "Edward", Age: 23, Gender: "Male"},
+    {id: "2021ict204", Name: "George", Age: 25, Gender: "Male"},
+    {id: "2021ict206", Name: "Eva", Age: 24, Gender: "Female"},
+    {id: "2021ict207", Name: "Sphoia", Age: 23, Gender: "Female"},
+    {id: "2021ict203", Name: "Neo", Age: 24, Gender: "Male"}
+];
