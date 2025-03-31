3a222158283caac93d7ba5fd4a396fc167b19b37
Isuru Udakara <pereraisuruudakara@gmail.com>
2025-03-31 11:32:03 +0530

    Corrected a mistake getPossiblePurchase

--- Problem1.js
+++ Problem1.js
@@ -19,4 +19,4 @@ function getPossiblePurchase(budget, keyboardPrices, mousePrices){
 
 console.log(getPossiblePurchase(60, [40, 50, 60], [5, 8, 12]));
 console.log(getPossiblePurchase(10, [3, 1], [5, 2, 8]));
-console.log(getMaxPurchase(20, [30, 15], [8, 10, 6])); 
+console.log(getPossiblePurchase(20, [30, 15], [8, 10, 6])); 
