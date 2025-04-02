072824891eadc0cf08b4d14374e946da74000946
Isuru Udakara <pereraisuruudakara@gmail.com>
2025-04-02 19:04:16 +0530

    Added example data to test the code

--- Problem3.js
+++ Problem3.js
@@ -5,3 +5,7 @@ function getMaxNumber(num){
     digits.sort((a, b) => b - a);
     return parseInt(digits.join(''), 10);
 }
+
+console.log(getMaxNumber(215)); 
+console.log(getMaxNumber(1093));  
+console.log(getMaxNumber(5));
