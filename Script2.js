d86bd15f882daa5c20b2a0abfe4f349918953596
Isuru Udakara <pereraisuruudakara@gmail.com>
2025-04-06 23:47:51 +0530

    Added message logged before reading file

--- Script2.js
+++ Script2.js
@@ -3,8 +3,9 @@ const fs = require('fs').promises;
 const readFile = (filepath) => {
     return fs.readFile(filepath, "utf-8"); // returns a promise
 }
-readFile("file.txt").then((data) => {
+readFile("read.txt").then((data) => {
     console.log(data); 
 }).catch((err) => {
     console.error(err);
 });
+console.log("File reading is done..");
