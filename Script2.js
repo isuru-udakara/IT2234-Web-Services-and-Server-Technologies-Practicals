9fc5a4e45acd67332f823373240d257554c3a052
Isuru Udakara <pereraisuruudakara@gmail.com>
2025-04-06 23:46:24 +0530

    Handling the promise and accessing file content

--- Script2.js
+++ Script2.js
@@ -3,3 +3,8 @@ const fs = require('fs').promises;
 const readFile = (filepath) => {
     return fs.readFile(filepath, "utf-8"); // returns a promise
 }
+readFile("file.txt").then((data) => {
+    console.log(data); 
+}).catch((err) => {
+    console.error(err);
+});
