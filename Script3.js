86d2c26ec4bcc963fbbe4d490213ba35c7bed6d3
Isuru Udakara <pereraisuruudakara@gmail.com>
2025-04-07 00:07:50 +0530

    Reading files using await method

--- Script3.js
+++ Script3.js
@@ -1,4 +1,15 @@
 const fs = require('fs').promises;
 const readFile= async() => {
-   
+    try{
+        const [data, data1]= await Promise.allSettled([
+            fs.readFile("file.txt", "utf-8"),
+            fs.readFile("data.txt", "utf-8")
+        ]);
+        console.log(data.value);
+        console.log(data1.value);
+        console.log(data.status);
+        console.log(data1.status);
+    }catch(err){
+        console.error(err);
+    }
 }
